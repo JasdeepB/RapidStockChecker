6820cb84045e93c8af5b1d0a37bc0955ee877e9d
Jasdeep <jasdeepbrar187@gmail.com>
2021-10-20 15:55:34 -0700

    Added FAQ, Updated language, and added buildspec.yml

--- RapidStockChecker/Controllers/HomeController.cs
+++ RapidStockChecker/Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace RapidStockChecker.Controllers
             return View();
         }
 
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
